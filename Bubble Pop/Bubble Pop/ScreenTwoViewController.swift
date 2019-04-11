e8f317d14f9db0fb28a01b265c421ebb97c8a11d
Binh Dang <magician0811@gmail.com.com>
2019-04-11 16:58:37 +1000

    init commit assignment 2

---
+++ Bubble Pop/Bubble Pop/ScreenTwoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ScreenTwoViewController.swift
+//  Bubble Pop
+//
+//  Created by Quang Binh Dang on 11/4/19.
+//  Copyright © 2019 UTS. All rights reserved.
+//
+
+import UIKit
+
+class ScreenTwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
